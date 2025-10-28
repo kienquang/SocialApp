4437a02b92af3a5491cc1fc5defe2239a1463628
Longke <ninjalong161@gmail.com>
2025-10-28 11:03:36 +0700

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,26 +2,15 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-=======
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
->>>>>>> 9cd027d0762677104ef93ecbcb0df18f335517fe
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-<<<<<<< HEAD
-=======
-    use WithoutModelEvents;
-
->>>>>>> 9cd027d0762677104ef93ecbcb0df18f335517fe
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-<<<<<<< HEAD
         // Gọi các seeder theo đúng thứ tự phụ thuộc
         $this->call([
             UsersSeeder::class,
@@ -33,13 +22,6 @@ class DatabaseSeeder extends Seeder
             MessageReadsSeeder::class,
             MentionsSeeder::class,
             // Các bảng report không có dữ liệu mẫu nên không cần gọi seeder
-=======
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
->>>>>>> 9cd027d0762677104ef93ecbcb0df18f335517fe
         ]);
     }
 }

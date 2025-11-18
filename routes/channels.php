0f45cc1d24ae1e35db9acacdb7462302abe815b3
Longke <ninjalong161@gmail.com>
2025-11-19 00:35:05 +0700

    xong thông báo

--- routes/channels.php
+++ routes/channels.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('converation.change.{id}', function ($user, $id) {
+Broadcast::channel('conversation.change.{id}', function ($user, $id) {
+    return (int)$user->id === (int)$id;
+});
+Broadcast::channel('notifications.{id}', function ($user, $id) {
     return (int)$user->id === (int)$id;
 });
 /*Broadcast::channel('message.sent.{id}', function ($user, $id) {
     return (int)$user->id === (int)$id;
 });*/
-Broadcast::routes(['middleware' => ['auth:sanctum']]); // hoặc 'auth' nếu dùng session
+
